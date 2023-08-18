ed3bb6b597ce133fe682fcef661641331a825c1e
joseangelmx <joseangelvazqueztorres@outlook.com>
2023-08-18 12:21:37 -0600

    se agrego la entidad videojuego

--- GuerrerosTeam.Core/Videogames/Videogame.cs
+++ GuerrerosTeam.Core/Videogames/Videogame.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GuerrerosTeam.Core.Videogames
 {
-    internal class Videogame
+    public class Videogame
     {
+        [Key]
+        public int Id { get; set; }
+        [Required] 
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Publisher { get; set;}
     }
 }
