f40c2b41aec1b4493ee4bbb12058807d9058e0ea
Joel Ramirez <joel.ramirez@puntosingular.com>
2023-08-18 14:32:39 -0500

    ApplicationService para la implementacion del servicio del CRUD

---
+++ GuerrerosTeam.ApplicationServices/Videogames/IVideogameAppService.cs
@@ -0,0 +1,22 @@
+﻿using GuerrerosTeam.Core.Videogames;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuerrerosTeam.ApplicationServices.Videogames
+{
+    public interface IVideogameAppService
+    {
+        Task<List<Videogame>> GetVideogameAsync();
+
+        Task<int> AddVideogameAsync(Videogame videogame);
+
+        Task DeleteVideogameAsync(int videogameId);
+
+        Task<Videogame> GetVideogameByIdAsync(int videogameId);
+
+        Task EditVideogameAsync(Videogame videogame);
+    }
+}
---
+++ GuerrerosTeam.ApplicationServices/Videogames/VideogameAppService.cs
@@ -0,0 +1,47 @@
+﻿using GuerrerosTeam.Core.Videogames;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuerrerosTeam.ApplicationServices.Videogames
+{
+    public class VideogameAppService: IVideogameAppService
+    {
+        //agregar la refrenecia al proyecto entiti framework con el nombre de la clase.
+        private readonly IRepository<int, Videogame> _repository;
+
+        public VideogameAppService(IRepository<int, Videogame> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> AddVideogameAsync(Videogame videogame)
+        {
+            await _repository.AddAsync(videogame);
+            return videogame.Id;
+        }
+
+        public async Task DeleteVideogameAsync(int videogameId)
+        {
+            await _repository.DeleteAsync(videogameId);
+        }
+
+        public async Task EditVideogameAsync(Videogame videogame)
+        {
+            await _repository.UpdateAsync(videogame);
+        }
+
+        public async Task<List<Videogame>> GetVideogameAsync()
+        {
+            //using Microsoft.EntityFrameworkCore; cuando de error el tolistasync
+            return await _repository.GetAll().ToListAsync();
+        }
+
+        public async Task<Videogame> GetVideogameByIdAsync(int videogameId)
+        {
+            return await _repository.GetAsync(videogameId);
+        }
+    }
+}
